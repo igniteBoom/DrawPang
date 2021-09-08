63dd5f8c7fe90f365a3772734b00a7fa6e2bf1d5
igniteBoom <parkbnmki@gmail.com>
2021-09-09 00:06:04 +0900

    [gamescene]gesture 그림 및 타임이 수정
    그림 휙 설정 다 하기

--- Assets/Script/Game/GestureGroup.cs
+++ Assets/Script/Game/GestureGroup.cs
@@ -9,10 +9,11 @@ public class GestureGroup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int tmpIndex = _GestureList.Count - 2;
+        int tmpIndex = _GestureList.Count - 2;          // 24 - 2 == 22
         for(int i = 0; i < _GestureList.Count; i++)
         {
-            _GestureList[i].transform.localPosition = new Vector3(_GestureList[i].GetComponent<RectTransform>().rect.width * (i - tmpIndex), 0,0);
+            _GestureList[i].transform.localPosition = new Vector3(_GestureList[i].GetComponent<RectTransform>().rect.width * (i - tmpIndex)
+                + (i - tmpIndex) * 10, 0,0);            // 0 - 22 * width       23 - 22 * width
             if(i >= _GestureList.Count - 3)
             {
                 _GestureList[i].SetActive(true);
@@ -51,15 +52,26 @@ public class GestureGroup : MonoBehaviour
         int tmpIndex = _GestureList.Count - 2;
         for (int i = 0; i < _GestureList.Count; i++)
         {
-            _GestureList[i].GetComponent<RectTransform>().DOAnchorPosX(_GestureList[i].GetComponent<RectTransform>().rect.width * (i - tmpIndex), 0.3f).SetEase(Ease.OutBounce);
-            if (i >= _GestureList.Count - 3)
+            _GestureList[i].GetComponent<RectTransform>().DOAnchorPosX(_GestureList[i].GetComponent<RectTransform>().rect.width * (i - tmpIndex) + (i - tmpIndex) * 10, 0.2f).SetEase(Ease.OutBounce);
+            if (i > _GestureList.Count - 3)
             {
                 _GestureList[i].SetActive(true);
+            }
+            else if(i == _GestureList.Count - 3)
+            {
+                StartCoroutine(delayFalseObject(0.1f, i));
+                //_GestureList[i].SetActive(false);
             }
             else
             {
                 _GestureList[i].SetActive(false);
             }
         }
+    }
+
+    IEnumerator delayFalseObject(float time, int index)
+    {
+        yield return new WaitForSeconds(time);
+        _GestureList[index].SetActive(true);
     }
 }
