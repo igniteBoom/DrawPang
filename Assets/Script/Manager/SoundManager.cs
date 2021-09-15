30d2e4f9df7097fa6f2a0b6b8f940bcae6cc4764
igniteBoom <46617160+igniteBoom@users.noreply.github.com>
2021-09-15 18:21:29 +0900

    soundmanager 스크립트 추가

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    protected SoundManager() { }
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,  // 아무것도 아님. 그냥 Sound enum의 개수 세기 위해 추가. (0, 1, '2' 이렇게 2개) 
+    }
+
+    AudioSource[] _audioSources = new AudioSource[(int)Sound.MaxCount];
+    Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
