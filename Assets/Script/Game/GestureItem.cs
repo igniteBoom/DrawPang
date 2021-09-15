d9a0c3a0f1e4ecc6bcac3b1cfc44f549536a863b
igniteBoom <parkbnmki@gmail.com>
2021-09-15 23:10:39 +0900

    이미지 갱신
    .

---
+++ Assets/Script/Game/GestureItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestureItem : MonoBehaviour
+{
+    public Sprite[] _gestureSprite;
+    private Image _myImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myImage = this.GetComponent<Image>(); 
+        if (null == _myImage) Debug.Log("Sprite Component null"); 
+        
+        _myImage.sprite = _gestureSprite[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
