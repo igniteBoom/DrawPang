ea285a4e984ec4fd8d220be10dad6d4b88be39f7
igniteBoom <parkbnmki@gmail.com>
2021-09-23 22:05:57 +0900

    안티 해커 커밋
    .

---
+++ Assets/Plugins/CodeStage/AntiCheatToolkit/Runtime/Scripts/Common/UnityApiResultsHolder.cs
@@ -0,0 +1,84 @@
+﻿#region copyright
+// -------------------------------------------------------
+// Copyright (C) Dmitriy Yukhanov [https://codestage.net]
+// -------------------------------------------------------
+#endregion
+
+#if !UNITY_WEBGL
+#define ACTK_ASYNC
+#endif
+
+namespace CodeStage.AntiCheat.Common
+{
+	using Storage;
+	using UnityEngine;
+
+	/// <summary>
+	/// User-friendly wrapper around few internally used Unity APIs which can't be accessed from background threads.
+	/// </summary>
+	/// You only need to touch this if you are going to use ObscuredFile / ObscuredFilePrefs from the background threads.
+	public static class UnityApiResultsHolder
+	{
+		private static string deviceUniqueIdentifier;
+		private static string persistentDataPath;
+
+		/// <summary>
+		/// Call this from the main thread before using ObscuredFile / ObscuredFilePrefs from the background threads.
+		/// </summary>
+		/// Calling this method avoids getting exceptions while working with ObscuredFile / ObscuredFilePrefs from the
+		/// background threads.
+		/// <param name="warmUpDeviceIdentifier">Pass true to init API needed for the Lock To Device feature
+		/// (SystemInfo.deviceUniqueIdentifier).
+		/// You need this to be true only when using ObscuredFile / ObscuredFilePrefs from the background threads
+		/// with DeviceLock enabled and without custom DeviceID set.
+		/// Passing true is similar to the DeviceIdHolder.ForceLockToDeviceInit() call, please read that API docs for
+		/// more information about possible side effects.</param>
+		public static void InitForAsyncUsage(bool warmUpDeviceIdentifier)
+		{
+			if (System.Threading.SynchronizationContext.Current == null)
+			{
+				Debug.LogError($"Please call {nameof(InitForAsyncUsage)} from main thread!");
+				return;
+			}
+
+			GetPersistentDataPath();
+
+			if (warmUpDeviceIdentifier)
+				GetDeviceUniqueIdentifier();
+		}
+
+		internal static string GetDeviceUniqueIdentifier()
+		{
+			if (string.IsNullOrEmpty(deviceUniqueIdentifier))
+			{
+#if ACTK_ASYNC
+				if (System.Threading.SynchronizationContext.Current != null)
+					deviceUniqueIdentifier = SystemInfo.deviceUniqueIdentifier;
+				else
+					throw new DeviceUniqueIdentifierException();
+#else
+				deviceUniqueIdentifier = SystemInfo.deviceUniqueIdentifier;
+#endif
+			}
+			
+			return deviceUniqueIdentifier;
+		}
+		
+		internal static string GetPersistentDataPath()
+		{
+			if (string.IsNullOrEmpty(persistentDataPath))
+			{
+#if ACTK_ASYNC
+				if (System.Threading.SynchronizationContext.Current != null)
+					persistentDataPath = Application.persistentDataPath;
+				else
+					throw new PersistentDataPathException();
+#else
+				persistentDataPath = Application.persistentDataPath;
+#endif
+			}
+			
+			return persistentDataPath;
+		}
+	}
+}
---
+++ Assets/Plugins/CodeStage/AntiCheatToolkit/Runtime/Scripts/Storage/File/ObscuredFileSettings.cs
@@ -0,0 +1,109 @@
+﻿#region copyright
+// ------------------------------------------------------
+// Copyright (C) Dmitriy Yukhanov [https://codestage.net]
+// ------------------------------------------------------
+#endregion
+
+namespace CodeStage.AntiCheat.Storage
+{
+	/// <summary>
+	/// Specifies file location.
+	/// </summary>
+	public enum ObscuredFileLocation : byte
+    {
+		/// <summary>
+		/// Corresponds to the <a href="https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html">Application.persistentDataPath</a>.
+		/// </summary>
+        PersistentData = 5,
+		
+		/// <summary>
+		/// Allows setting custom file path.
+		/// </summary>
+        Custom = 10,
+    }
+
+	/// <summary>
+	/// Specific settings to use with ObscuredFile instance.
+	/// </summary>
+	public class ObscuredFileSettings
+	{
+		/// <summary>
+		/// Controls file location. Read more at #ObscuredFileLocation docs.
+		/// </summary>
+		public ObscuredFileLocation LocationKind { get; }
+		
+		/// <summary>
+		/// Controls file encryption settings. Read more at #EncryptionSettings docs.
+		/// </summary>
+        public EncryptionSettings EncryptionSettings { get; private set; }
+
+		/// <summary>
+		/// Controls DeviceLock feature settings. Read more at #DeviceLockSettings docs.
+		/// </summary>
+		public DeviceLockSettings DeviceLockSettings { get; private set; }
+
+		/// <summary>
+		/// Controls anti-tampering setting.
+		/// </summary>
+		/// When enabled, data integrity is validated and invokes NotGenuineDataDetected event on violation.
+		/// <br/>Disable to skip integrity validation entirely and improve performance.
+		public bool ValidateDataIntegrity { get; set; }
+
+		/// <summary>
+		/// Creates default settings instance.
+		/// </summary>
+		/// Default settings are:
+		/// - LocationKind set to ObscuredFileLocation.PersistentData
+		/// - EncryptionSettings.ObscurationMode set to ObscurationMode.Plain
+		/// - DeviceLockSettings.Level set to DeviceLockLevel.None
+		/// - ValidateDataIntegrity set to true
+		public ObscuredFileSettings():this(ObscuredFileLocation.PersistentData)
+		{
+		}
+
+		/// <summary>
+		/// Creates settings instance with specified LocationKind.
+		/// </summary>
+		/// Default settings are:
+		/// - EncryptionSettings.ObscurationMode set to ObscurationMode.Plain
+		/// - DeviceLockSettings.Level set to DeviceLockLevel.None
+		/// - ValidateDataIntegrity set to true
+		public ObscuredFileSettings(ObscuredFileLocation locationKind) : this(
+			new EncryptionSettings(), new DeviceLockSettings(), locationKind) { }
+
+		/// <summary>
+		/// Creates settings instance with specified DeviceLockSettings.
+		/// </summary>
+		/// Default settings are:
+		/// - LocationKind set to ObscuredFileLocation.PersistentData
+		/// - EncryptionSettings.ObscurationMode set to ObscurationMode.Plain
+		/// - ValidateDataIntegrity set to true
+		public ObscuredFileSettings(DeviceLockSettings deviceLockSettings) : this(
+			new EncryptionSettings(), deviceLockSettings)
+		{
+;
+		}
+
+		/// <summary>
+		/// Creates user-specified settings instance.
+		/// </summary>
+		public ObscuredFileSettings(EncryptionSettings encryptionSettings, DeviceLockSettings deviceLockSettings,
+			ObscuredFileLocation locationKind = ObscuredFileLocation.PersistentData, bool validateDataIntegrity = true)
+		{
+			EncryptionSettings = encryptionSettings;
+			DeviceLockSettings = deviceLockSettings;
+			LocationKind = locationKind;
+			ValidateDataIntegrity = validateDataIntegrity;
+		}
+
+		/*public void UpdateSettings(EncryptionSettings settings)
+		{
+			EncryptionSettings = settings;
+		}
+		
+		public void UpdateSettings(DeviceLockSettings settings)
+		{
+			DeviceLockSettings = settings;
+		}*/
+	}
+}
